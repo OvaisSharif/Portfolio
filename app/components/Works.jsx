64bef2d6bf29c04e7677d83266b7b87f665ab6d1
Ovais Sharif <sharifovais61@gmail.com>
2025-01-24 23:00:23 +0530

    created my services section

---
+++ app/components/Works.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Works = () => {
+  return (
+    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+      <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
+       <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
+
+       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        I am a frontend Engineer with 2.4 years of experience in multiple
+         companies like Graymatrix Solutions and MAK Byte.</p>
+    </div>
+  )
+}
+
+export default Works
